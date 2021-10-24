f376e7d2eb0d59b510e7eaa7ce75ae906dcf1f53
jmutt78 <jmcintosh@docusites.com>
2021-10-24 16:10:24 -0700

    inverse text link complete

--- src/components/atoms/Link/Link.js
+++ src/components/atoms/Link/Link.js
@@ -8,55 +8,63 @@ import classes from '../Button/Button.module.css';
  * @param {*} props
  */
 const Link = (props) => {
-    const { variant = 'primary', children, href, ...rest } = props;
+  const { variant = 'primary', children, href, ...rest } = props;
 
-    let buttonClass = null;
+  let buttonClass;
 
-    switch (variant) {
-        case 'primary': {
-            buttonClass = classes.primary;
+  switch (variant) {
+    case 'primary': {
+      buttonClass = classes.primary;
 
-            break;
-        }
-
-        case 'secondary': {
-            buttonClass = classes.secondary;
-
-            break;
-        }
-
-        case 'tiertary': {
-            buttonClass = classes.tiertary;
-
-            break;
-        }
-
-        default: {
-            buttonClass = null;
-        }
+      break;
     }
 
-    return (
-        <a
-            href={href}
-            className={buttonClass}
-            {...rest}
-        >
-            {children}
-        </a>
-    );
+    case 'secondary': {
+      buttonClass = classes.secondary;
+
+      break;
+    }
+
+    case 'tiertary': {
+      buttonClass = classes.tiertary;
+
+      break;
+    }
+
+    case 'inverse': {
+      buttonClass = classes.inverse;
+
+      break;
+    }
+
+    default: {
+      buttonClass = null;
+    }
+  }
+
+  return (
+    <a href={href} className={buttonClass} {...rest}>
+      {children}
+    </a>
+  );
 };
 
 Link.propTypes = {
-    active: PropTypes.string,
-    children: PropTypes.string.isRequired,
-    disabled: PropTypes.bool,
-    href: PropTypes.string.isRequired,
-    variant: PropTypes.oneOf(['text', 'primary', 'secondary', 'tiertary'])
+  active: PropTypes.string,
+  children: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  href: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf([
+    'text',
+    'primary',
+    'secondary',
+    'tiertary',
+    'inverse',
+  ]),
 };
 
 Link.defaultProps = {
-    variant: 'text'
+  variant: 'text',
 };
 
 export default Link;
--- src/components/atoms/Link/Link.stories.js
+++ src/components/atoms/Link/Link.stories.js
@@ -3,40 +3,60 @@ import React from 'react';
 import Link from './Link';
 
 export default {
-    title: 'Atoms/Buttons/Link',
-    component: Link,
-    argTypes: {
-        variant: {
-            control: {
-                type: 'select',
-                options: ['primary', 'secondary', 'text']
-            }
-        },
-        active: {
-            control: {
-                type: 'select',
-                options: [false, true]
-            }
-        }
-    }
+  title: 'Atoms/Buttons/Link',
+  component: Link,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'text', 'inverse'],
+      },
+    },
+    active: {
+      control: {
+        type: 'select',
+        options: [false, true],
+      },
+    },
+  },
 };
 
 const Template = (args) => <Link {...args} />;
 
+const InverseTemplate = (args) => (
+  <div
+    style={{
+      backgroundColor: '#460273',
+      padding: `${30}px`,
+    }}
+  >
+    <Link {...args} />
+  </div>
+);
+
 export const TextLink = Template.bind({});
 TextLink.args = {
-    variant: 'text',
-    children: 'Text link',
-    disabled: false,
-    active: false,
-    href: '#'
+  variant: 'text',
+  children: 'Text link',
+  disabled: false,
+  active: false,
+  href: '#',
+};
+
+export const TextLinkInverse = InverseTemplate.bind({});
+TextLinkInverse.args = {
+  variant: 'inverse',
+  children: 'Inverse Text link',
+  disabled: false,
+  active: false,
+  href: '#',
 };
 
 export const LinkAsButton = Template.bind({});
 LinkAsButton.args = {
-    variant: 'secondary',
-    children: 'Link as button',
-    disabled: false,
-    active: false,
-    href: '#'
+  variant: 'secondary',
+  children: 'Link as button',
+  disabled: false,
+  active: false,
+  href: '#',
 };
